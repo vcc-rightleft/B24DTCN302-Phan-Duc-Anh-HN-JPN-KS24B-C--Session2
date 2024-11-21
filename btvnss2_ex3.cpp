f1a36b6e2bca517eee8778aebb7121946b8dea6e
vcc-rightleft <trumfaifai00ngo@gmail.com>
2024-11-21 09:58:03 +0700

    Add files via upload

---
+++ btvnss2_ex3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int a = 2, b = 5 
+	float thuong; 
+	result = a + b;
+	printf(" Tong:: %.2f\n",result);
+	result = a - b; 
+	printf(" hieu:: %.2f\n",result);
+	result = a * b;
+	printf(" Tich:: %.2f\n",result);
+	result = (float ) a/b; 
+	printf(" Thuong:: %.2f\n",result)
+	return 0;
+} 
---
+++ btvnss2_ex5.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int dai; 10
+	int rong; 2 
+	printf("%d",(dai+rong)*2);
+	printf ("\r%d",dai+rong);
+	return 0; 
+}	
